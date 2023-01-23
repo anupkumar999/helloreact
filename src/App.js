dfa65d970a5e970c4ede44893f6aa637544aef9a
Anup Kumar <anupkumar@Anups-MacBook-Pro.local>
2023-01-23 22:46:41 +0530

    this is my commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO THIS IS MY FIRST APP
         </a>
       </header>
     </div>
